2b1873bfb53af7f69f81378ef3bfa30f4efd0dcc
infra-ff0000 <ravenofpromise@gmail.com>
2015-12-21 15:54:21 -0800

    pushing what I have quit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+angular.module('app',['ui.router']).config(function($stateProvider, $urlRouterProvider){
+
+	$urlRouterProvider.otherwise('/home');
+
+	$stateProvider.state('home', {
+		url: '/home', templateUrl:'/templates/home.html',
+		controller:'HomeController'})
+
+	.state('resume', {
+		url: '/resume', templateUrl:'/templates/resume.html',
+		controller:'ResumeController'})
+
+	.state('portofolio', {
+		url: '/portfolio', templateUrl:'/templates/portfolio.html',
+		controller:'PortfolioController'});
+});
